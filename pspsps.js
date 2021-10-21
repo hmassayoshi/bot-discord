89968c8dcdee770d1d46d67830d9ccd7e394d1b2
paulo.hirami <paulo.hirami@inspira.com.br>
2021-10-21 14:43:58 -0300

    correções do git ignore

--- pspsps.js
+++ pspsps.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const { prefix, token } = require("./config.json");
+const { prefix, token } = require("./token.json");
 const ytdl = require("ytdl-core");
 const yts = require( 'yt-search');
 const client = new Discord.Client();
